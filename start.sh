dae5223490a12750d87b63aa4c680a57a58a6bca
Rahul Thakoor <rahul.thakoor@gmail.com>
2021-09-16 17:46:09 +0400

    Change logo and latency mode detection

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/vc/lib
 
 DEVICE_NAME="${AIRPIPLAY_NAME:="Air-Pi-Play@$HOSTNAME"}"
 AUDIO_OUT="${AIRPIPLAY_AUDIO_OUT:="hdmi"}"
 BACKGROUND="${AIRPIPLAY_BACKGROUND:="auto"}"
-if $AIRPIPLAY_LATENCY_MODE -eq 1 ; then LATENCY="-l"; else LATENCY=""; fi
+case $AIRPIPLAY_LATENCY_MODE in
+  1) LATENCY="-l";;
+  0) LATENCY="";;
+esac
 ROTATION="-r ${AIRPIPLAY_ROTATION:=""}"
 FLIP="-f ${AIRPIPLAY_FLIP:=""}"
 
